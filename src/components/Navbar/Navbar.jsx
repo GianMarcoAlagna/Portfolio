1787e030eb3d799db3d80247329e1a8bcb18b6e7
GianMarcoAlagna <marcoalagna1@gmail.com>
2024-04-22 22:53:46 -0500

    Fixed issue where the nav drawer was being overlayed by the footer, resulting in it being cut off at the bottom of the page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,42 +40,44 @@ export const Navbar = ({ lenis }) => {
   }, [padding]);
 
   return (
-    <nav className="navbar border-after" style={style}>
-      <div
-        className='navbar__left'
-      >
-        <RxHamburgerMenu
-          size={30}
-          className="navbar__hamburger"
-          onClick={toggleDrawer}
-        />
-        <header className="navbar__header">
-          <span
-            className="navbar__header__text"
-            onClick={scrollToTop}
-          >
-            Gian-Marco Alagna
-          </span>
-        </header>
-      </div>
-      <div
-        className="navbar__center"
-      >
-        <header className="navbar__header center">
-          <span
-            className="navbar__header__text"
-            onClick={scrollToTop}
-          >
-            {screen.width > 836 ? "Gian-Marco Alagna" : "GMA"}
-          </span>
-        </header>
-        <NavLinks links={links} />
-      </div>
-      <div
-        className="navbar__right"
-      >
-        <ColorMode />
-      </div>
+    <>
+      <nav className="navbar border-after" style={style}>
+        <div
+          className='navbar__left'
+        >
+          <RxHamburgerMenu
+            size={30}
+            className="navbar__hamburger"
+            onClick={toggleDrawer}
+          />
+          <header className="navbar__header">
+            <span
+              className="navbar__header__text"
+              onClick={scrollToTop}
+            >
+              Gian-Marco Alagna
+            </span>
+          </header>
+        </div>
+        <div
+          className="navbar__center"
+        >
+          <header className="navbar__header center">
+            <span
+              className="navbar__header__text"
+              onClick={scrollToTop}
+            >
+              {screen.width > 836 ? "Gian-Marco Alagna" : "GMA"}
+            </span>
+          </header>
+          <NavLinks links={links} />
+        </div>
+        <div
+          className="navbar__right"
+        >
+          <ColorMode />
+        </div>
+      </nav>
       <div className={`navbar__drawer${drawerOpen ? " open" : ""}`} ref={drawerRef}>
         <RxHamburgerMenu
           className={"navbar__hamburger"}
@@ -91,7 +93,7 @@ export const Navbar = ({ lenis }) => {
           </a>
         ))}
       </div>
-    </nav>
+    </>
   )
 }
 
