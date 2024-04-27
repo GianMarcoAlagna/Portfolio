0f0e67b42715d41ed930512ad1ce3a6b8f1ac0a4
GianMarcoAlagna <marcoalagna1@gmail.com>
2024-04-27 10:44:28 -0500

    Added tech stack icons and better footer content sizing thru media queries

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,26 @@
 import { Contact } from "../Contact/Contact";
 import { Resume } from "../Resume/Resume";
 import { Reveal } from "../Reveal/Reveal";
+import { FaArrowUp } from "react-icons/fa";
 import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer className="footer border-before">
       <Reveal>
-        <footer className="footer__content">
+        <div className="footer__content">
           <Resume />
           <Contact />
-        </footer>
+        </div>
+        <div>
+          <p>
+            Made with ❤️ by <a href="https://github.com/GianMarcoAlagna">Gian Marco Alagna</a>
+          </p>
+          <img src="https://skillicons.dev/icons?perline=6&i=js,react,html,css,p5js,vite,vscode,git,github,aws,postgresql" alt="Tech Stack" />
+          <p>
+            <FaArrowUp /> Tech Stack Icons by <a href="https://skillicons.dev/">Skill Icons</a> <FaArrowUp />
+          </p>
+        </div>
       </Reveal>
     </footer>
   );
