1aeda9efed85f70799ba30059c3fdee6cec944c5
GianMarcoAlagna <marcoalagna1@gmail.com>
2024-08-01 17:20:33 -0500

    Fixed bug where Card component was being given a ref directly instead of it being passed down

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,49 @@
+import { useEffect, useState } from 'react';
 import Lenis from '@studio-freight/lenis';
-import { Main, Navbar } from './components'
+import { Main, Navbar } from './components';
 import { MainProvider } from './context/MainContext';
 import { Footer } from "./components/Footer/Footer";
 import { Introduction } from './components';
 import { Preloader } from './components/Preloader/Preloader';
-import './App.css'
-import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   const lenis = new Lenis();
   const [loading, setLoading] = useState(true);
 
   function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
+    lenis.raf(time);
+    requestAnimationFrame(raf);
   }
-  requestAnimationFrame(raf)
+
+  requestAnimationFrame(raf);
 
   useEffect(() => {
-    (() => {
+    const handleLoad = () => {
       console.log('loaded');
       setLoading(false);
-    })();
+    };
+
+    if (document.readyState === 'complete') {
+      handleLoad();
+    } else {
+      window.addEventListener('load', handleLoad);
+      return () => window.removeEventListener('load', handleLoad);
+    }
   }, []);
 
   return (
     <>
       <MainProvider>
         <Preloader loading={loading}>
-          <Navbar
-          />
-          <Introduction
-            lenis={lenis}
-            dynamic={true}
-          />
-          <Main
-            lenis={lenis}
-          />
+          <Navbar />
+          <Introduction lenis={lenis} dynamic={true} />
+          <Main lenis={lenis} />
           <Footer />
         </Preloader>
       </MainProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
