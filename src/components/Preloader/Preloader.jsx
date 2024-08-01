f1b4ea711d9de5bc0caa789f44bafd7f07ff3157
GianMarcoAlagna <marcoalagna1@gmail.com>
2024-08-01 16:36:21 -0500

    Testing a fix for infinite load

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Preloader.css';
 
-export const Preloader = ({ children, loading}) => (
+export const Preloader = ({ children, loading }) => (
   loading ? (
     <div className="preloader">
       <div className="spinner"></div>
     </div>
-  ) : children
+  ) : (
+    <>{children}</>
+  )
 );
